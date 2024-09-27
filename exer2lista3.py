1ccecc09c74d7cae24602ef9d036290d0a6868ee
KwMajor <147251890+KwMajor@users.noreply.github.com>
2024-09-27 11:29:23 -0300

    att lista 3

--- exer2lista3.py
+++
@@ -1,7 +0,0 @@
-usuario = str(input("usuario: "))
-senha = str(input("senha: "))
-while usuario == senha:
-    print("erro")
-    usuario = str(input("usuario"))
-    senha = str(input("senha"))
-print("usuario e senha correto!")
